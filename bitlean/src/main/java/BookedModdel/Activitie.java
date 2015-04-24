f42a8a825b520e1534eff3ec90c2ce67c3fcb5c1
Sebastian Graveleij <siksebbe@hotmail.com>
2015-04-24 10:33:24 +0200

    added new moddel classes

---
+++ bitlean/src/main/java/BookedModdel/Activitie.java
@@ -0,0 +1,17 @@
+package BookedModdel;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Activitie {
+
+    public String comment;
+    public String date;
+    public int durationInKm;
+    public int durationInMinutes;
+    public String id;
+    public String source;
+    public String status;
+    public String subType;
+    public String type;
+    public Booking booking;
+}
