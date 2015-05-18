02765c5318bd7c13808c3ca43afba6e80ad08fb0
Erik Welander <erik.welander@hotmail.com>
2015-05-19 01:06:38 +0200

    Another major overhauwl, merging group code

---
+++ LeanbitSats/app/src/main/java/se/leanbit/sats/fragments/CustomCircleDraw.java
@@ -0,0 +1,259 @@
+package se.leanbit.sats.fragments;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import se.leanbit.sats.R;
+
+/**
+ * Created by gina on 2015-05-13.
+ */
+public class CustomCircleDraw extends View
+{
+    private final Rect textBounds = new Rect();
+    private boolean mDrawCircleFill = true;
+    private Paint mPaintFill;
+    private Paint mPaintEmpty;
+    private Paint mPaintDivider;
+    private Paint mPaintRectangle;
+    private Paint mPaintText;
+    private Paint mPaintWeekDates;
+    private int mWidth;
+    private int mHeight;
+    private float mCircleSize;
+    private String mWeekdates;
+    private float mTopBarHeight;
+    private float mBottomBarHeight;
+    private int mMaxAntalPass;
+    private Bitmap mPinkMarker;
+    private int mAntalPass;
+    private float mSegmentHeight;
+    private float mCircleStroke;
+    private int mPassNextWeek;
+    private int mPassLastWeek;
+    private float mPinkMarkerSize;
+    private Boolean mIsPastWeek;
+    private Boolean mIsCurrentWeek;
+
+    public CustomCircleDraw(Context context, AttributeSet attrs)
+    {
+        super(context);
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.CustomCircleDraw,
+                0, 0);
+        init();
+    }
+
+    private void init()
+    {
+        mPaintFill = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintEmpty = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintDivider = new Paint((Paint.ANTI_ALIAS_FLAG));
+        mPaintText = new Paint((Paint.ANTI_ALIAS_FLAG));
+        mPaintRectangle = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintWeekDates = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintText.setColor(Color.WHITE);
+        mPaintFill.setColor(getResources().getColor(R.color.circle_orange));
+        mPaintEmpty.setColor(getResources().getColor(R.color.circle_orange));
+        mPaintEmpty.setStyle(Paint.Style.STROKE);
+        mCircleStroke = getResources().getDimension(R.dimen.circle_width);
+        mPaintEmpty.setStrokeWidth(mCircleStroke);
+        mCircleSize = getResources().getDimension(R.dimen.circle_size);
+        mPaintDivider.setColor(Color.GRAY);
+        mPaintDivider.setStrokeWidth(getResources().getDimension(R.dimen.stroke_width) / 2);
+        mPaintText.setTextSize(getResources().getDimension(R.dimen.size_of_text));
+        mPaintWeekDates.setTextSize(getResources().getDimension(R.dimen.size_of_text));
+        mPaintRectangle.setColor(Color.WHITE);
+        mPaintRectangle.setStyle(Paint.Style.FILL);
+        mAntalPass = 0;
+        mMaxAntalPass = 0;
+        mIsCurrentWeek = false;
+        mTopBarHeight = getResources().getDimension(R.dimen.height_of_top_rectangle);
+        mBottomBarHeight = getResources().getDimension(R.dimen.height_of_bottom_rectangle);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.now_marker);
+        mPinkMarkerSize = getResources().getDimension(R.dimen.pink_marker_size);
+        mPinkMarker = Bitmap.createScaledBitmap(bitmap,(int)mPinkMarkerSize,(int)mPinkMarkerSize,false);
+        mIsCurrentWeek = true;
+        mIsPastWeek = false;
+
+    }
+
+    public void drawCircleFill(boolean drawCircleFill)
+    {
+        mDrawCircleFill = drawCircleFill;
+        invalidate();
+        requestLayout();
+    }
+
+
+    protected void onDraw(Canvas canvas)
+    {
+        super.onDraw(canvas);
+
+        canvas.drawRect(0, 0, mWidth, mTopBarHeight, mPaintRectangle);
+
+        drawLines(canvas);
+        if(mIsCurrentWeek){
+            drawCirclesBeforeLine(canvas);
+        }
+        if(mIsPastWeek){
+           drawCirclesBeforeLine(canvas);
+           drawCirclesAfterLine(canvas);
+        }
+        if (mIsPastWeek)
+        {
+            canvas.drawCircle(mWidth / 2, circlePosition(), mCircleSize, mPaintFill);
+            drawTextCentred(canvas, mPaintText, "" + mAntalPass, mWidth / 2, circlePosition());
+        } else
+        {
+            canvas.drawCircle(mWidth / 2, circlePosition(), mCircleSize - mCircleStroke / 2, mPaintEmpty);
+            mPaintText.setColor(Color.BLACK);
+            drawTextCentred(canvas, mPaintText, "" + mAntalPass, mWidth / 2, circlePosition());
+        }
+        if (mIsCurrentWeek)
+        {
+            canvas.drawBitmap(mPinkMarker, (mWidth / 2) - mPinkMarkerSize/2, mTopBarHeight -mPinkMarkerSize/4, null);
+        }
+        canvas.drawRect(0, mHeight - mBottomBarHeight, mWidth, mHeight, mPaintRectangle);
+        canvas.drawLine(0, mHeight - mBottomBarHeight, mWidth, mHeight - mBottomBarHeight, mPaintDivider);
+        drawTextCentred(canvas, mPaintWeekDates, mWeekdates, mWidth / 2, mHeight - (mBottomBarHeight) / 2); //weekdatesText
+        canvas.drawLine(0, mHeight-1, mWidth, mHeight-1, mPaintDivider);
+    }
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh)
+    {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        Log.d("onSizeChanged", "sizeChanged...............x");
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        mWidth = MeasureSpec.getSize(widthMeasureSpec);
+        mHeight = MeasureSpec.getSize(heightMeasureSpec);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    public void drawTextCentred(Canvas canvas, Paint mPaintText, String text, float cx, float cy)
+    {
+        mPaintText.getTextBounds(text, 0, text.length(), textBounds);
+        canvas.drawText(text, cx - textBounds.exactCenterX(), cy - textBounds.exactCenterY(), mPaintText);
+    }
+
+    public void drawLines(Canvas canvas)
+    {
+        mSegmentHeight = (mHeight - (mTopBarHeight + mBottomBarHeight)) / (mMaxAntalPass + 1);
+        if (mMaxAntalPass < 7)
+        {
+            for (int i = 0; i < mMaxAntalPass + 1; i++)
+            {
+                canvas.drawLine(0, (mSegmentHeight * (i)) + mTopBarHeight, mWidth, (mSegmentHeight * (i)) + mTopBarHeight, mPaintDivider);
+            }
+        } else
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                canvas.drawLine(0, (mSegmentHeight * (i)), mWidth, (mSegmentHeight * (i)), mPaintDivider);
+            }
+        }
+
+    }
+
+    public void setWeekDates(String weekdates)
+    {
+        mWeekdates = weekdates;
+        invalidate();
+        requestLayout();
+    }
+
+    public void setMaxAntalPass(int maxAntalPass)
+    {
+        this.mMaxAntalPass = maxAntalPass;
+        invalidate();
+        requestLayout();
+    }
+
+    public void setIsCurrentWeek(Boolean isCurrentWeek)
+    {
+        this.mIsCurrentWeek = isCurrentWeek;
+        invalidate();
+        requestLayout();
+    }
+
+    void setAntalPass(int antalPass)
+    {
+        this.mAntalPass = antalPass;
+        invalidate();
+        requestLayout();
+    }
+
+    private void drawCirclesAfterLine(Canvas canvas)
+    {
+        canvas.drawLine(mWidth / 2, circlePosition()
+                , mWidth + ((mWidth / 2)), nextCirclePos(), mPaintEmpty);
+    }
+
+    private void drawCirclesBeforeLine(Canvas canvas)
+    {
+        canvas.drawLine(mWidth / 2, circlePosition(), -(mWidth / 2), lastCirclePos(), mPaintEmpty);
+    }
+    private void drawCirclesCurrentLine(Canvas canvas)
+    {
+        canvas.drawLine(mWidth / 2-mCircleSize, circlePosition(), -(mWidth / 2), lastCirclePos(), mPaintEmpty);
+    }
+    private float nextCirclePos()
+    {
+        if (mPassNextWeek == -1)
+        {
+            return mHeight - mBottomBarHeight - (mSegmentHeight * mAntalPass);
+        }
+        return mHeight - mBottomBarHeight - (mSegmentHeight * mPassNextWeek);
+    }
+
+    private float lastCirclePos()
+    {
+        if (mPassLastWeek == -1)
+        {
+            return mHeight - mBottomBarHeight - (mSegmentHeight * mAntalPass);
+        }
+        return mHeight - mBottomBarHeight - (mSegmentHeight * mPassLastWeek);
+    }
+
+    float circlePosition()
+    {
+        return mHeight - mBottomBarHeight - (mSegmentHeight * mAntalPass);
+    }
+
+    public void setPassNextWeek(int passNextWeek)
+    {
+        this.mPassNextWeek = passNextWeek;
+        invalidate();
+        requestLayout();
+    }
+
+    public void setPassLastWeek(int passLastWeek)
+    {
+        this.mPassLastWeek = passLastWeek;
+        invalidate();
+        requestLayout();
+    }
+    public void isPastWeek(Boolean isPastWeek){
+        this.mIsPastWeek = isPastWeek;
+        invalidate();
+        requestLayout();
+    }
+
+}
--- LeanbitSats/app/src/main/java/se/leanbit/sats/fragments/PagerFragment.java
+++ LeanbitSats/app/src/main/java/se/leanbit/sats/fragments/PagerFragment.java
@@ -1,5 +1,6 @@
 package se.leanbit.sats.fragments;
 
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,7 +10,12 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import java.util.ArrayList;
+
 import se.leanbit.sats.R;
+import se.leanbit.sats.models.SatsActivity;
+import se.leanbit.sats.repositories.services.SatsActivitiesService;
+import se.leanbit.sats.repositories.services.SatsTimeFormatService;
 
 /**
  * Created by gina on 2015-05-12.
@@ -21,26 +27,37 @@ public class PagerFragment extends Fragment
 
         Bundle args = getArguments();
         int position = args.getInt("page_position");
-
-        LinearLayout fragmentLayout = (LinearLayout)inflater.inflate(R.layout.pager_fragment, container, false);
-        LinearLayout mainView = (LinearLayout)fragmentLayout.findViewById(R.id.main_view);
-        LinearLayout markedCell = (LinearLayout)fragmentLayout.findViewById(R.id.cell_2);
-
-        changeColor(position, mainView);
-        LinearLayout circleCell = (LinearLayout)inflater.inflate(R.layout.pager_circle_cell, container, false);
-        markedCell.addView(circleCell);
+        CustomCircleDraw fragmentLayout = (CustomCircleDraw)inflater.inflate(R.layout.pager_draw_circle, container, false);
+        setFillOnCircle(position,fragmentLayout);
+        fragmentLayout.setWeekDates(args.getString("week_dates"));
+        fragmentLayout.setIsCurrentWeek(args.getBoolean("is_current_week"));
+        fragmentLayout.setMaxAntalPass(args.getInt("max_antal_pass"));
+        fragmentLayout.setAntalPass(args.getInt("pass_per_vecka"));
+        fragmentLayout.setPassNextWeek(args.getInt("pass_next_week"));
+        fragmentLayout.setPassLastWeek(args.getInt("pass_last_week"));
+        fragmentLayout.isPastWeek(args.getBoolean("is_past_Week"));
+        changeColor(position, fragmentLayout);
         return fragmentLayout;
     }
 
+    private void setFillOnCircle(int position,CustomCircleDraw circleView)
+    {
+        if(position%2==0){
+            circleView.drawCircleFill(true);
+        }else{
+            circleView.drawCircleFill(false);
+        }
 
+    }
 
     private View changeColor(int position, View view)
     {
         if(position%2==0){
-            view.setBackgroundColor(Color.GRAY);
+            view.setBackgroundColor(getResources().getColor(R.color.scroll_view_lightgrey));
         }else{
-            view.setBackgroundColor(Color.WHITE);
+            view.setBackgroundColor(getResources().getColor(R.color.scroll_view_darkgrey));
         }
         return view;
     }
+
 }
