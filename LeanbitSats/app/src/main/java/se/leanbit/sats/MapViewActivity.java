1d1d9eccf81aed4902c4d638c92a80f6bd394bca
Gina Adamova <gina@Ginas-MacBook-Pro.local>
2015-05-27 10:29:37 +0200

    addd map and webview for sats centers

--- LeanbitSats/app/src/main/java/se/leanbit/sats/MapViewActivity.java
+++ LeanbitSats/app/src/main/java/se/leanbit/sats/MapViewActivity.java
@@ -1,17 +1,20 @@
 package se.leanbit.sats;
 
-import android.app.ActionBar;
-import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -19,36 +22,66 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.zip.Inflater;
 
 import se.leanbit.sats.R;
+import se.leanbit.sats.models.SatsActivity;
+import se.leanbit.sats.models.SatsSimpleCenter;
 import se.leanbit.sats.repositories.services.SatsActivitiesService;
 
 /**
- * Created by gina on 2015-05-22.
- */
+* Created by gina on 2015-05-22.
+*/
 public class MapViewActivity extends ActionBarActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, LocationListener, GoogleMap.OnCameraChangeListener
 {
     private GoogleMap mMap = null;
     private Location mLastLocation;
     private Map<String, Marker> mSatsMarkers;
+    private Map<String, SatsSimpleCenter> mAllSatsCenters;
     private LatLng mCameraPosition;
     private final static String CAMERA_LAT = "lat_coords";
     private final static String CAMERA_LANG = "lang_coords";
     private SatsActivitiesService mSatsActivitiesService;
+    private Map<String, Marker> mMarkers;
+    private LayoutInflater inflater;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar1);
+        ImageView image = (ImageView) findViewById(R.id.action_bar_logo_settings);
+        setupClickListner(image);
+        image.setImageResource(R.drawable.back_icon);
+        TextView textView = (TextView) findViewById(R.id.action_bar_text_training);
+        textView.setText("HITTA CENTER");
         setSupportActionBar(toolbar);
         setupLocation();
         drawMap();
+        inflater = getLayoutInflater();
         mSatsActivitiesService = new SatsActivitiesService();
+        mAllSatsCenters = mSatsActivitiesService.getFullCenterMap();
+    }
+
+    private void setupClickListner(ImageView image)
+    {
+        image.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                view.getContext().startActivity(intent);
+            }
+        });
     }
 
     private void drawMap()
@@ -72,7 +105,9 @@ public class MapViewActivity extends ActionBarActivity implements OnMapReadyCall
         } else {
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mLatLng, 15));
         }
-        //Log.d(mCameraPosition.toString(), "onMapReady fired ..............");
+        addMarkers();
+        setInfoClickListener();
+        Log.d("     ", "onMapReady fired ..............");
     }
     private void setupLocation() {
         if (!isGooglePlayServicesAvailable()) {
@@ -86,6 +121,7 @@ public class MapViewActivity extends ActionBarActivity implements OnMapReadyCall
             onLocationChanged(mLastLocation);
         }
         locationManager.requestLocationUpdates(bestProvider, 60000, 0, (android.location.LocationListener) this);
+        Log.d("     ", "setupLocation fired ..............");
     }
     private boolean isGooglePlayServicesAvailable() {
         int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
@@ -101,8 +137,8 @@ public class MapViewActivity extends ActionBarActivity implements OnMapReadyCall
     public void onLocationChanged(Location location) {
         double latitude = location.getLatitude();
         double longitude = location.getLongitude();
-        LatLng latLng = new LatLng(latitude, longitude);
-        Log.d("onLocationChanged", "onLocationChanged fired ..............");
+        mCameraPosition = new LatLng(latitude, longitude);
+        Log.d(mCameraPosition.latitude +" lat " + mCameraPosition.longitude, "onLocationChanged fired ..............");
 
     }
     @Override
@@ -110,9 +146,78 @@ public class MapViewActivity extends ActionBarActivity implements OnMapReadyCall
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
         Log.d("onMapClick", "onMapClick fired ..............");
     }
+
+    private void addMarkers() {
+      mMarkers = new HashMap<>();
+        for (Map.Entry<String, SatsSimpleCenter> entry : mAllSatsCenters.entrySet()) {
+            if(entry.getValue().lat < mCameraPosition.latitude +0.1 & entry.getValue().Long < mCameraPosition.longitude +0.1 )
+
+            mMarkers.put(entry.getValue().centerName, mMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(entry.getValue().lat, entry.getValue().Long))
+                    .title("SATS " + entry.getValue().centerName)
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.sats_pin_normal))));
+
+        }
+
+        Log.d("onAddMarkers", " allMarkerssize" + mAllSatsCenters.size() + " lat"+ mMarkers.size()+ " long "+mCameraPosition.longitude);
+
+    }
+    private void setInfoClickListener() {
+        setCustomWindow();
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener()
+        {
+
+            @Override
+            public void onInfoWindowClick(Marker marker)
+            {
+                String centerName = marker.getTitle();
+                String url = "";
+                centerName = centerName.substring(5);
+                for (Map.Entry<String, SatsSimpleCenter> entry : mAllSatsCenters.entrySet())
+                {
+                    if (centerName.equals(entry.getValue().centerName))
+                    {
+                        url = entry.getValue().webUrl;
+                    }
+                }
+                Bundle bundle = new Bundle();
+                bundle.putString("urlString", url);
+                Intent intent = new Intent(MapViewActivity.this, SatsWebActivity.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+                Log.d("setInfoClickListener " + url + " " + centerName, " onInfoClick fired ..............");
+            }
+        });
+    }
+
+    private void setCustomWindow()
+    {
+
+        GoogleMap.InfoWindowAdapter customWindowAdapter = new GoogleMap.InfoWindowAdapter()
+        {
+
+            @Override
+            public View getInfoWindow(Marker marker)
+            {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker)
+            {
+                View popup=inflater.inflate(R.layout.custom_info_window, null);
+
+                TextView tv=(TextView)popup.findViewById(R.id.text_sats_center);
+                tv.setText(marker.getTitle());
+                return(popup);
+            }
+        };
+        mMap.setInfoWindowAdapter(customWindowAdapter);
+    }
+
     @Override
     public void onCameraChange(CameraPosition cameraPosition) {
-       // mCameraPosition = mMap.getCameraPosition().target;
+        mCameraPosition = mMap.getCameraPosition().target;
     }
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -126,6 +231,14 @@ public class MapViewActivity extends ActionBarActivity implements OnMapReadyCall
 
     @Override
     public void onProviderDisabled(String provider) {
+
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putFloat(CAMERA_LAT, (float) mCameraPosition.latitude);
+        outState.putFloat(CAMERA_LANG, (float) mCameraPosition.longitude);
+        Log.d(Float.toString((float) mCameraPosition.latitude), "onSavedInstanceState fired ..............................");
+        super.onSaveInstanceState(outState);
 
     }
 
